14b78729b188587ab6c7711c8145e1f70dd53098
davimatiassg <davimatiassg@gmail.com>
2025-11-07 09:28:47 -0300

    Win_1 Dialog registry

--- scripts/game_management/GameDialogs.cs
+++ scripts/game_management/GameDialogs.cs
@@ -5,10 +5,10 @@ using Godot.Collections;
 [GlobalClass]
 public partial class GameDialogs : Resource
 {
-[Export]
-public Dictionary DialogData = new Dictionary
+
+public static Dictionary DialogData = new Dictionary
 {
-    {"First Defeat", new Dictionary
+    {"Loss_1", new Dictionary
         { {0, new Dictionary {
                     { "icon", "res://icon.svg" },
                     { "title", "character name" },
@@ -25,7 +25,7 @@ public Dictionary DialogData = new Dictionary
         }
 
     },
-    {"Second Defeat", new Dictionary
+    {"Loss_2", new Dictionary
         { {0, new Dictionary {
                     { "icon", "res://icon.svg" },
                     { "title", "character name" },
@@ -42,7 +42,7 @@ public Dictionary DialogData = new Dictionary
         }
 
     },
-    {"Third Defeat", new Dictionary
+    {"Loss_3", new Dictionary
         { {0, new Dictionary {
                     { "icon", "res://icon.svg" },
                     { "title", "character name" },
@@ -59,7 +59,118 @@ public Dictionary DialogData = new Dictionary
         }
 
     },
-    {"First Victory", new Dictionary
+    {"Win_1", new Dictionary
+        { {0, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "???" },
+                    { "dialog", "Se é o que deseja, espie." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" },
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            },
+            {1, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "???" },
+                    { "dialog", "Através da porta." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" },//TODO! colocar imagem da porta em tela cheia
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            },
+            {2, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "???" },
+                    { "dialog", "Por de trás dessa porta, há o que era você." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" },//TODO! por imagem da fechadura em tela cheia
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            },
+            {3, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "???" },
+                    { "dialog", "Não se deixe levar por algo tão vago como uma vida." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" },//TODO! por imagem do homem, blurry, através da fechadura
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            },
+            {4, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "???" },
+                    { "dialog", "Lembre-se de porque você está aqui." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" },//TODO! por imagem do homem de costas no limbo
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            },
+
+            {5, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "Introspecção" },
+                    { "dialog", "Você tem a sensação de familiaridade, como ao ver uma foto de muito tempo atrás, tão antiga que nem se lembra mais quando foi tirada." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" },//TODO! por imagem do homem de costas no limbo
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            },
+
+            {6, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "" },
+                    { "dialog", "Pela primeira vez desde que você chegou na sala, você consegue sentir suas mãos." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" },
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            },
+
+            { 7, new Dictionary {
+                    { "icon", "res://icon.svg" },
+                    { "title", "" },
+                    { "dialog", "Elas querem [shake rate=20.0 level=5 connected=1][b]continuar jogando[/b][/shake]." },
+                    { "method", new Dictionary
+                        {
+                            { "object", new GodotObject() },
+                            { "methodPath", "" }, //TODO: REMOVER IMAGEM DA TELA
+                            { "args", new Godot.Collections.Array()}
+                        }
+                    }
+                }
+            }
+        }
+
+    },
+    {"Win_2", new Dictionary
         { {0, new Dictionary {
                     { "icon", "res://icon.svg" },
                     { "title", "character name" },
@@ -76,24 +187,7 @@ public Dictionary DialogData = new Dictionary
         }
 
     },
-    {"Second Victory", new Dictionary
-        { {0, new Dictionary {
-                    { "icon", "res://icon.svg" },
-                    { "title", "character name" },
-                    { "dialog", "dialog" },
-                    { "method", new Dictionary
-                        {
-                            { "object", new GodotObject() },
-                            { "methodPath", "" },
-                            { "args", new Godot.Collections.Array()}
-                        }
-                    }
-                }
-            },
-        }
-
-    },
-    {"Third Victory", new Dictionary
+    {"Win_3", new Dictionary
         { {0, new Dictionary {
                     { "icon", "res://icon.svg" },
                     { "title", "character name" },
--- scripts/game_management/GameManager.cs
+++ scripts/game_management/GameManager.cs
@@ -32,15 +32,23 @@ public partial class GameManager : Node
         }
     }
 
-    public static void RoundEnd(bool playerWon)
+    public static async void RoundEnd(bool playerWon)
     {
         TurnState.IsRoundRunning = false;
         OnStopGame?.Invoke(playerWon);
 
         var tween = Instance.CreateTween();
         tween.TweenInterval(0.5);
-        tween.TweenCallback(Callable.From(()=> { Tabletop.Instance.AnimateBoardTransition(false, 0.5); }));
+        tween.TweenCallback(Callable.From(() => { Tabletop.Instance.AnimateBoardTransition(false, 0.5); }));
 
+        if (playerWon) PlayerScore++;
+        else EnemyScore++;
+
+        await DialogMessenger.SpawnDialog((Godot.Collections.Dictionary)GameDialogs.DialogData[playerWon ? "Win_" + PlayerScore : "Loss_" + EnemyScore]);
+
+        //TODO! something, idk, show the images I guess;
+
+        RoundStart(); 
         
     }
 
@@ -51,7 +59,10 @@ public partial class GameManager : Node
 
         var tween = Instance.CreateTween();
         tween.TweenInterval(0.5);
-        tween.TweenCallback(Callable.From(()=> { Tabletop.Instance.AnimateBoardTransition(); }));
+        tween.TweenCallback(Callable.From(() => { Tabletop.Instance.AnimateBoardTransition(); }));
+        
+           
+
     }
 
     public override void _Ready()
